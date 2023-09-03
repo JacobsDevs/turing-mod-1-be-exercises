2cfbc472b10146a76bcda8325158edaf9a537668
Jacob <jacobphelan@hotmail.com>
2023-09-04 09:08:03 +1000

    test: Add RSpec for Contestant class

---
+++ practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
@@ -0,0 +1,34 @@
+require './lib/contestant'
+require './lib/game'
+
+RSpec.describe Contestant do
+	describe '#initialize' do
+		it 'exists and has parameters' do
+			alexander = Contestant.new({first_name: 'Alexander',
+				last_name: 'Aigiades',
+				age: 28,
+				state_of_residence: 'CO',
+				spending_money: 10})
+			expect(alexander.full_name).to eq('Alexander Aigiades')
+			expect(alexander.age).to eq(28)
+			expect(alexander.state_of_residence).to eq('CO')
+			expect(alexander.spending_money).to eq(10)
+		  expect(alexander.out_of_state?).to eq(false)
+			expect(alexander.game_interests).to eq([])
+		end
+	end
+	describe '#add_game_interest' do
+		it 'adds game name to the game_interests variable' do
+			alexander = Contestant.new({first_name: 'Alexander',
+				last_name: 'Aigiades',
+				age: 28,
+				state_of_residence: 'CO',
+				spending_money: 10})
+			mega_millions = Game.new('Mega Millions', 5, true)
+			pick_4 = Game.new('Pick 4', 2)
+			alexander.add_game_interest(mega_millions)
+			alexander.add_game_interest(pick_4)
+			expect(alexander.game_interests).to eq(["Mega Millions", "Pick 4"])
+		end
+	end
+end
