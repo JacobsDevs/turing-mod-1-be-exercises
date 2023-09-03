122edde34cf1b80e689b1d1357481afc987f45e8
Jacob <jacobphelan@hotmail.com>
2023-09-04 09:17:41 +1000

    feat: Add Contestant#initialize

--- practice_independent_challenges/week4/colorado_lottery/lib/contestant.rb
+++ practice_independent_challenges/week4/colorado_lottery/lib/contestant.rb
@@ -2,7 +2,8 @@ class Contestant
 	attr_reader :full_name,
 							:age,
 							:state_of_residence,
-							:spending_money
+							:spending_money,
+							:game_interests
 
   def initialize(data)
 		@first_name = data[:first_name]
--- practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
+++ practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
@@ -14,6 +14,15 @@ RSpec.describe Contestant do
 			expect(alexander.state_of_residence).to eq('CO')
 			expect(alexander.spending_money).to eq(10)
 			expect(alexander.game_interests).to eq([])
+		end
+	end
+	describe '#out_of_state?' do
+		it 'returns if contestant is resident of CO or not' do
+			alexander = Contestant.new({first_name: 'Alexander',
+				last_name: 'Aigiades',
+				age: 28,
+				state_of_residence: 'CO',
+				spending_money: 10})
 			expect(alexander.out_of_state?).to eq(false)
 		end
 	end
