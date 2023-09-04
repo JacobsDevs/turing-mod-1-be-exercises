79e9a3f0e81df3d8830e2abf87c3f53c0365feb2
Jacob <jacobphelan@hotmail.com>
2023-09-04 10:12:15 +1000

    feat: Add ColoradoLottery#can_register? & valid_location?

--- practice_independent_challenges/week4/colorado_lottery/lib/colorado_lottery.rb
+++ practice_independent_challenges/week4/colorado_lottery/lib/colorado_lottery.rb
@@ -12,4 +12,12 @@ class ColoradoLottery
 	def interested_and_18?(contestant, game)
 		contestant.age >= 18 && contestant.game_interests.include?(game.name)
 	end
+
+	def can_register?(contestant, game)
+	  interested_and_18?(contestant, game) && valid_location?(contestant, game)
+	end
+
+	def valid_location?(contestant, game)
+		contestant.state_of_residence == 'CO' || game.national_drawing?
+	end
 end
