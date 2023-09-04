2b4e0c127b2b4e179da265e4346bb4770ca7aa5f
Jacob <jacobphelan@hotmail.com>
2023-09-04 15:45:12 +1000

    test: Add RSpec for Curator iteration 2

---
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -0,0 +1,74 @@
+RSpec.describe Curator do
+	describe '#initialize' do
+		it 'exists and has an empty photographs array' do
+			curator = Curator.new
+			expect(curator).to be_an_instance_of(Curator)
+			expect(curator.photographs).to eq([])
+			expect(curator.artists).to eq([])
+		end
+	end
+
+	describe 'Methods' do
+		it 'can #add_photograph' do
+			curator = Curator.new
+			photo_1 = Photograph.new({
+				id: "1",      
+				name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+				artist_id: "1",      
+				year: "1954"      
+			})
+			
+			photo_2 = Photograph.new({
+						id: "2",      
+						name: "Moonrise, Hernandez",      
+						artist_id: "2",      
+						year: "1941"      
+			})
+
+			curator.add_photograph(photo1)
+			curator.add_photograph(photo2)
+			expect(curator.photographs).to eq([photo1, photo2])
+		end
+	  
+		it 'can #add_artist' do
+		  curator = Curator.new
+			artist_1 = Artist.new({
+				id: "1",      
+				name: "Henri Cartier-Bresson",      
+				born: "1908",      
+				died: "2004",      
+				country: "France"      
+				})
+				artist_2 = Artist.new({
+						id: "2",      
+						name: "Ansel Adams",      
+						born: "1902",      
+						died: "1984",      
+						country: "United States"      
+				})
+				curator.add_artist(artist_1)
+				curator.add_artist(artist_2)
+				expect(curator.artists).to eq([artist_1, artist_2])
+		end
+
+		it 'can #find_artist_by_id' do
+			curator = Curator.new
+			artist_1 = Artist.new({
+				id: "1",      
+				name: "Henri Cartier-Bresson",      
+				born: "1908",      
+				died: "2004",      
+				country: "France"      
+				})
+				artist_2 = Artist.new({
+						id: "2",      
+						name: "Ansel Adams",      
+						born: "1902",      
+						died: "1984",      
+						country: "United States"      
+				})
+				curator.add_artist(artist_1)
+				curator.add_artist(artist_2)
+				expect(curator.find_artist_by_id("1")).to eq(artist_1)
+	end
+end
