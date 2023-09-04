def5668096eb050a7656c454191b4c7d379aa4b1
Jacob <jacobphelan@hotmail.com>
2023-09-04 16:05:19 +1000

    test: Add RSpec for Curator iteration 3

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -71,5 +71,76 @@ RSpec.describe Curator do
 				curator.add_artist(artist_2)
 				expect(curator.find_artist_by_id("1")).to eq(artist_1)
 		end
+		before(:each) do
+			@curator = Curator.new
+			@artist_1 = Artist.new({
+				id: "1",      
+				name: "Henri Cartier-Bresson",      
+				born: "1908",      
+				died: "2004",      
+				country: "France"      
+				})
+				@artist_2 = Artist.new({
+						id: "2",      
+						name: "Ansel Adams",      
+						born: "1902",      
+						died: "1984",      
+						country: "United States"      
+				})
+				@artist_3 = Artist.new({
+					id: "3",      
+					name: "Willy Ronis",      
+					born: "1910",      
+					died: "2009",      
+					country: "France"      
+					})
+			@curator.add_artist(@artist_1)
+			@curator.add_artist(@artist_2)
+			@curator.add_artist(@artist_3)
+			@photo_1 = Photograph.new({
+				id: "1",      
+				name: "Portrait of Alberto Giacometti",      
+				artist_id: "1",      
+				year: "1938"      
+			})
+			@photo_2 = Photograph.new({
+				id: "2",      
+				name: "Behind the Gare Saint-Lazare",      
+				artist_id: "1",      
+				year: "1932"      
+			})
+			@photo_3 = Photograph.new({
+				id: "3",      
+				name: "Place Vendome",      
+				artist_id: "3",      
+				year: "1947"      
+			})
+			@photo_4 = Photograph.new({
+				id: "4",      
+				name: "Moon Over Half Dome",      
+				artist_id: "2",      
+				year: "1960"      
+			})
+			@photo_5 = Photograph.new({
+				id: "5",      
+				name: "Oak Tree, Snow Storm, Yosemite, from Portfolio One: Twelve Photographic Prints",      
+				artist_id: "2",      
+				year: "1948"      
+			})
+			@curator.add_photograph(@photo_1)
+			@curator.add_photograph(@photo_2)
+			@curator.add_photograph(@photo_3)
+			@curator.add_photograph(@photo_4)
+			@curator.add_photograph(@photo_5)
+		it 'can return a hash of all #artists_and_photographs' do
+			expect(@curator.artists_and_photographs).to eq({@artist_1=>[@photo_1, @photo_2], @artist_2=>[@photo_3], @artist_3=>[@photo_4, @photo_5]})
+		end
+		it 'can return an array of #artists_with_multiple_photographs' do
+		  expect(@curator.artists_with_multiple_photographs).to eq([@artist_1, @artist_3])
+		end
+		it 'can return an array of #photographs_from_country' do
+      expect(@curator.photographs_from_country("France")).to eq([@photo_1, @photo_2, @photo_3])
+			expect(@curator.photographs_from_country("United States")).to eq([@photo_4, @photo_5])
+		end
 	end
 end
